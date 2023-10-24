335531d432bd37600f13995d44dc50d8de2a33a1
Aditya <aditya.singh@shivit.com>
2023-10-24 09:47:16 +0530

    Create category list and add page, create view page of quiz

---
+++ src/app/pages/admin/view-quizzes/view-quizzes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-view-quizzes',
+  templateUrl: './view-quizzes.component.html',
+  styleUrls: ['./view-quizzes.component.css']
+})
+export class ViewQuizzesComponent implements OnInit {
+
+  quizzes:any = [];
+
+  constructor(private _quiz: QuizService) { }
+
+  ngOnInit(): void {
+    this._quiz.getQuizzes().subscribe((data:any)=>{
+      this.quizzes = data;
+    },
+    (error)=>{
+      Swal.fire('Error !!', 'Error during loading data !', 'error');
+    });
+  }
+
+}
